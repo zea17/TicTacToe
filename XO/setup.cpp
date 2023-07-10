e23bdb3248cbbb1e4c0dcced07336cfa747708f0
zea <93328643+zea17@users.noreply.github.com>
2023-07-10 15:13:53 +0330

    added comment in choose number of players function

--- XO/setup.cpp
+++ XO/setup.cpp
@@ -11,10 +11,18 @@ int choose_dimention(){
     return size;
 }
 
+// This function prompts the user to choose the number of players for a Tic-Tac-Toe game
+// and returns the entered number as an integer.
+
 int choose_number_of_players() {
   int number_of_players;
-
+    
+// Use a while loop to repeatedly prompt the user for input until a valid input
+    
   while (true) {
+      
+// Display the options for the number of players
+ 
     std::cout << "Choose the number of players:" << std::endl;
     std::cout << "1 - Play against the computer" << std::endl;
     std::cout << "2 - Play against a friend" << std::endl;
@@ -25,6 +33,9 @@ int choose_number_of_players() {
       case 2:
         return number_of_players;
       default:
+            
+// If the input is anything else, display an error message and ask the user to enter the number of players again
+
         std::cout << "Invalid input. Please enter 1 or 2." << std::endl;
     }
   }
