f202df922010abd2e3860481b84b48fb65f1968f
zea <93328643+zea17@users.noreply.github.com>
2023-07-15 18:44:32 +0330

    added setup flow manager

--- source/main.cpp
+++ source/main.cpp
@@ -3,10 +3,5 @@
 
 int main()
 {
-    choose_dimension();
-    if (choose_number_of_players() == 1)
-    {
-
-        chooseGameLevel();
-    }
+    setup();
 }
--- source/setup.cpp
+++ source/setup.cpp
@@ -14,7 +14,6 @@ int choose_dimension()
   int size;
   std::cout << "Enter the dimensions of the Tic-Tac-Toe board: ";
   std::cin >> size;
-  clean_screen();
   return size;
 }
 
@@ -39,11 +38,8 @@ int choose_number_of_players()
     {
     case 1:
     case 2:
-      clean_screen();
-      return number_of_players;
     case 3:
-      clean_screen();
-      return choose_dimension();
+      return number_of_players;
     default:
       std::cout << "Invalid input. Please enter 1 or 2." << std::endl;
     }
@@ -53,7 +49,7 @@ int choose_number_of_players()
 // This function prompts the user to choose the game level for a Tic-Tac-Toe game
 // and returns the entered level as an integer (1 for Easy, 2 for Medium, 3 for Hard).
 
-int chooseGameLevel()
+int choose_game_level()
 {
   int choice;
 
@@ -69,16 +65,44 @@ int chooseGameLevel()
     switch (choice)
     {
     case 1:
-      return 1;
     case 2:
-      return 2;
     case 3:
-      return 3;
     case 4:
-      clean_screen();
-      return choose_number_of_players();
+      return choice;
     default:
-      std::cout << "Invalid input. Please enter 1, 2, or 3." << std::endl;
+      std::cout << "Invalid input. Please enter 1, 2, or 3." << std::endl
+                << std::endl;
     }
+  }
+}
+
+void setup()
+{
+  int level = 1;
+  while (level < 4)
+  {
+    clean_screen();
+
+    if (level == 1)
+    {
+      choose_dimension();
+    }
+    if (level == 2)
+    {
+      int number_of_players = choose_number_of_players();
+      if (number_of_players == 3)
+      {
+        level -= 2;
+      }
+    }
+    if (level == 3)
+    {
+      int game_level = choose_game_level();
+      if (game_level == 4)
+      {
+        level -= 2;
+      }
+    }
+    level++;
   }
 }
