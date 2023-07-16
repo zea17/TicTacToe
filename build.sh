78d2029e0182bb1e3faa33b2221cccf18364541c
zea <93328643+zea17@users.noreply.github.com>
2023-07-16 14:21:03 +0330

    added storage manager file

--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@
 
 g++ -c source/main.cpp -o bin/main.o
 g++ -c source/setup.cpp -o bin/setup.o
+g++ -c source/storage_manager.cpp -o bin/storage_manager.o
 
-g++ bin/main.o bin/setup.o -o bin/ttt
+g++ bin/main.o bin/setup.o bin/storage_manager.o -o bin/ttt
 
 clear
 ./bin/ttt
