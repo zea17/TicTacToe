1101f0c0e4bd35345beb5c83c71b167d957c1213
zea <93328643+zea17@users.noreply.github.com>
2023-07-19 18:07:55 +0330

    added load grid state and save grid state

--- source/storage_manager.cpp
+++ source/storage_manager.cpp
@@ -7,17 +7,28 @@
 
 #define SETTING_FILE_PATH "~/.zeattt.txt"
 
-//
-
 bool does_state_exists() {
   std::ifstream settings_file;
   settings_file.open(SETTING_FILE_PATH, std::ios::in);
   return !settings_file.fail();
 }
-
+// line 1 = dimension
+// line 2 = number of players
+// line 3 = game level
+//
 void load_state() {
   std::fstream settings_file;
   settings_file.open(SETTING_FILE_PATH, std::ios::in);
+}
+
+void load_grid_state() {
+  std::ifstream setting_file(SETTING_FILE_PATH);
+
+  for (int row = 0; row < dimension; row++) {
+    setting_file.getline(grid[row], dimension);
+  }
+
+  setting_file.close();
 }
 
 void save_state() {
@@ -25,4 +36,15 @@ void save_state() {
   setting_file << dimension << std::endl
                << number_of_players << std::endl
                << game_level << std::endl;
+  setting_file.close();
+}
+
+void save_grid_state() {
+  std::ofstream setting_file(SETTING_FILE_PATH);
+
+  for (int row = 0; row < dimension; row++) {
+    std::cout << grid[row] << std::endl;
+  }
+
+  setting_file.close();
 }
