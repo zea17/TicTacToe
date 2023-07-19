438aff6d93868553159d97124aadb8c1166658ef
zea <93328643+zea17@users.noreply.github.com>
2023-07-19 18:11:59 +0330

    complete load state

--- source/storage_manager.cpp
+++ source/storage_manager.cpp
@@ -17,8 +17,10 @@ bool does_state_exists() {
 // line 3 = game level
 //
 void load_state() {
-  std::fstream settings_file;
-  settings_file.open(SETTING_FILE_PATH, std::ios::in);
+  std::ifstream setting_file(SETTING_FILE_PATH);
+  setting_file >> dimension >> number_of_players >> game_level;
+  load_grid_state();
+  setting_file.close();
 }
 
 void load_grid_state() {
@@ -36,6 +38,7 @@ void save_state() {
   setting_file << dimension << std::endl
                << number_of_players << std::endl
                << game_level << std::endl;
+  save_grid_state();
   setting_file.close();
 }
 
