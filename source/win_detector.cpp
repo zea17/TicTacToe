b4136e51f838ead8cf17749a2a48360cc659467f
zea <93328643+zea17@users.noreply.github.com>
2023-07-20 19:26:19 +0330

    added function to check vertical column tie

--- source/win_detector.cpp
+++ source/win_detector.cpp
@@ -1,4 +1,20 @@
 #include "win_detector.hpp"
 #include "globals.hpp"
+#include "grid.hpp"
 
-char
+int check_vertical_tie() {
+  for (int column = 0; column < dimension; column++) {
+    int first_element = grid[0][column];
+    bool is_tie = true;
+    for (int row = 1; row < dimension; row++) {
+      if (grid[row][column] != first_element) {
+        is_tie = false;
+        break;
+      }
+    }
+    if (is_tie) {
+      return column;
+    }
+  }
+  return -1;
+}
