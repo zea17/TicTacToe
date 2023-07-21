2b373fad9ca29343e4ab11332b0e40711b21d823
zea <93328643+zea17@users.noreply.github.com>
2023-07-21 16:06:39 +0330

    added globals variable

--- source/globals.cpp
+++ source/globals.cpp
@@ -4,3 +4,7 @@ int dimension = 3;
 int number_of_players = 1;
 int game_level = 1;
 char **grid;
+int winning_column = -1;
+int winning_row = -1;
+bool winning_ltr_diagonal = false;
+bool winning_rtl_diagonal = true;
--- source/globals.hpp
+++ source/globals.hpp
@@ -1,5 +1,13 @@
+#define X_VALUE 'X'
+#define O_VALUE 'O'
+#define EMPTY_VALUE '-'
 extern int dimension;
 extern int number_of_players;
 extern int game_level;
 
 extern char **grid;
+
+extern int winning_column;
+extern int winning_row;
+extern bool winning_ltr_diagonal;
+extern bool winning_rtl_diagonal;
