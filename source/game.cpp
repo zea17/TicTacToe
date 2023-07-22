ad4e76c9d536e6ce6d96788148c0f2f829ecbead
zea <93328643+zea17@users.noreply.github.com>
2023-07-22 18:47:21 +0330

    create prompt and play for user function

--- source/game.cpp
+++ source/game.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+
 #include "game.hpp"
+#include "globals.hpp"
+#include "grid.hpp"
+#include "terminal.hpp"
+
+void prompt_and_play_for_user(char xo) {
+  int cell_number = prompt_user_for_play(xo);
+  if (get_cell_at(cell_number) == EMPTY_VALUE)
+    set_cell_at(cell_number, xo);
+  else {
+    std::cout << "cell" << cell_number
+              << " is already filled please try another number" << std::endl;
+    prompt_and_play_for_user(xo);
+  }
+}
+
+void game_loop() {
+  while (true) {
+    clean_screen();
+    render_game();
+  }
+}
--- source/grid.hpp
+++ source/grid.hpp
@@ -3,3 +3,4 @@ void initialize_grid();
 void allocate_grid();
 void create_grid();
 char get_cell_at(int);
+void set_cell_at(int, char);
