88480d34908aef10211b1aa340ee6446058241a3
zea <93328643+zea17@users.noreply.github.com>
2023-07-23 14:38:42 +0330

    added play with easy ai function

--- source/ai.cpp
+++ source/ai.cpp
@@ -4,10 +4,26 @@
 
 #include "ai.hpp"
 #include "globals.hpp"
+#include "grid.hpp"
 
-void play_with_easy_ai() {}
+void play_with_easy_ai(char xo) {
+  int max_number = dimension * dimension;
+  while (true) {
+    int number = generate_random(MIN_NUMBER, max_number);
+    if (get_cell_at(number) == EMPTY_VALUE) {
+      set_cell_at(number, xo);
+      break;
+    }
+  }
+}
 
 int generate_random(int min, int max) {
   srand(time(NULL));
   return min + rand() % (max - min + 1);
+}
+
+void play_with_ai(char xo) {
+  if (game_level == 1) {
+    play_with_easy_ai(xo);
+  }
 }
