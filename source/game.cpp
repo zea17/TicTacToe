8a7ce45cf46991d0d01c8d505e1afbe8f94e1204
zea <93328643+zea17@users.noreply.github.com>
2023-07-23 16:12:05 +0330

    new game loop

--- source/game.cpp
+++ source/game.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 
+#include "ai.hpp"
 #include "game.hpp"
 #include "globals.hpp"
 #include "grid.hpp"
+#include "storage_manager.hpp"
 #include "terminal.hpp"
 #include "win_detector.hpp"
 
@@ -18,27 +20,46 @@ void prompt_and_play_for_user(char xo) {
   }
 }
 
+bool show_win_message_if_win_exist() {
+  char winner;
+  if ((winner = detect_win()) == EMPTY_VALUE) {
+    return false;
+  }
+
+  render_game();
+  std::cout << "winner is : " << winner << " :)" << std::endl;
+
+  return true;
+}
+
 void game_loop() {
   char winner = EMPTY_VALUE;
+
   while (true) {
     clean_screen();
     render_game();
-    prompt_and_play_for_user(X_VALUE);
-    if ((winner = win_detector()) != EMPTY_VALUE) {
-      std::cout << "winner is : " << winner << " :)" << std::endl;
+
+    // If it's X's Turn
+    if (current_turn == X_VALUE) {
+      prompt_and_play_for_user(X_VALUE);
+      current_turn = O_VALUE;
+    }
+
+    // If it's O's Turn
+    else {
+      if (number_of_players == 2) {
+        prompt_and_play_for_user(O_VALUE);
+      } else {
+        play_with_ai(O_VALUE);
+      }
+      current_turn = X_VALUE;
+    }
+
+    if (show_win_message_if_win_exist()) {
+      delete_state();
       break;
     }
-    render_game();
-    if (number_of_players == 2) {
-      prompt_and_play_for_user(O_VALUE);
-      render_game();
 
-      if ((winner = win_detector()) != EMPTY_VALUE) {
-        std::cout << "winner is : " << winner << " :)" << std::endl;
-        break;
-      }
-
-      win_detector();
-    }
+    save_state();
   }
 }
