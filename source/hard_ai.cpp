bcb44a0f85fb5d19f90c60eb6c86ca4e1401ad98
zea <93328643+zea17@users.noreply.github.com>
2023-07-24 17:49:06 +0330

    added random corner index

--- source/hard_ai.cpp
+++ source/hard_ai.cpp
@@ -1,3 +1,4 @@
+#include "ai.hpp"
 #include "globals.hpp"
 #include "grid.hpp"
 #include "medium_ai.hpp"
@@ -9,12 +10,25 @@ int get_center() { return dimension / 2; }
 void play_first_move(char xo) { grid[0][0] = xo; }
 
 bool play_second_move(char xo) {
-  if (dimension_is_odd()) {
-    int center_index = get_center();
+  int center_index = get_center();
+  if (grid[center_index][center_index] == EMPTY_VALUE && dimension_is_odd()) {
     grid[center_index][center_index] = xo;
     return true;
   }
-  grid[dimension - 1][(dimension - 1)] = xo;
+  grid[0][dimension - 1] = xo;
+}
+
+int random_corner_index() {
+  if (generate_random(0, 1) == 0) {
+    return 0;
+  }
+  return dimension - 1;
+}
+
+bool play_with_fork_strategy() {
+  for (int row = 0; row < dimension; row++) {
+    /* code */
+  }
 }
 
 void play_with_hard_ai(char xo) {
@@ -22,6 +36,9 @@ void play_with_hard_ai(char xo) {
 
   if (moves_count == 0) {
     play_first_move(xo);
-    return;
+  }
+
+  else if (moves_count <= 2) {
+    play_second_move(xo);
   }
 }
