5239918d13f689210aa7e9a1c9908af371045571
zea <93328643+zea17@users.noreply.github.com>
2023-07-24 20:23:22 +0330

    fixed the bug with adding break in switch case

--- source/ai.cpp
+++ source/ai.cpp
@@ -5,6 +5,7 @@
 #include "easy_ai.hpp"
 #include "globals.hpp"
 #include "grid.hpp"
+#include "hard_ai.hpp"
 #include "medium_ai.hpp"
 
 int generate_random(int min, int max) {
@@ -23,7 +24,12 @@ void play_with_ai(char xo) {
   switch (game_level) {
   case 1:
     play_with_easy_ai(xo);
+    break;
   case 2:
     play_with_medium_ai(xo);
+    break;
+  case 3:
+    play_with_hard_ai(xo);
+    break;
   }
 }
