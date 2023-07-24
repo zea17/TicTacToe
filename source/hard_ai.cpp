b4847eb7a3b895d707ca50efbef9e0cc6f1e7fa4
zea <93328643+zea17@users.noreply.github.com>
2023-07-24 17:21:28 +0330

    added secon move function

--- source/hard_ai.cpp
+++ source/hard_ai.cpp
@@ -2,7 +2,19 @@
 #include "grid.hpp"
 #include "medium_ai.hpp"
 
+bool dimension_is_odd() { return dimension % 2 == 1; }
+
+int get_center() { return dimension / 2; }
+
 void play_first_move(char xo) { grid[0][0] = xo; }
+
+bool play_second_move(char xo) {
+  if (dimension_is_odd()) {
+    int center_index = get_center();
+    grid[center_index][center_index] = xo;
+    return true;
+  }
+}
 
 void play_with_hard_ai(char xo) {
   int moves_count = count_played_moves();
